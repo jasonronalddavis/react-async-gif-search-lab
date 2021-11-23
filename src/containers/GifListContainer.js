74e50c84cd7d7fbc622657f94999996e9aea42c5
jasonronalddavis <jdavis@c.ringling.edu>
2021-11-22 21:00:02 -0600

    Done.

---
+++ src/containers/GifListContainer.js
@@ -0,0 +1,49 @@
+
+import React, { Component } from 'react'
+import GifList from '../components/GifList'
+import GifSearch from '../components/GifSearch'
+
+
+
+class GifListContainer extends Component {
+    state = {
+        gifs: []
+    }
+
+
+
+
+
+    render(){
+        return(
+            <div>
+                <GifSearch fetchGIFs={this.fetchGIFs} />
+                <GifList gifs={this.state.gifs} />
+            </div>
+        )
+    }
+
+    fetchGIFs = (query = "dolphins") => {
+        fetch(`https://media3.giphy.com/media/l3fQbSe8heZeznfAk/giphy.gif?cid=d057e149nkedmktkfqnq5whnddx5oqpci30x5zjywci31966&rid=giphy.gif&ct=g&limit=3`)
+        then(res => res.json())
+        .then(({data}) => {
+            this.setState({ gifs: data.map( gif => ({ url: gif.images.original.url }) ) })
+        })
+
+    }
+
+
+    componentDidMount(){
+        this.fetchGIFs()
+    }
+
+
+}
+
+
+export default GifListContainer 
+
+
+//https://api.giphy.com/v1/gifs/search?q=dolphin&api_key=YAkvRsSSbx8NLZeQcX2Mww1uoz4EFBLB&rating=g
+
+//"https://media3.giphy.com/media/l3fQbSe8heZeznfAk/giphy.gif?cid=d057e149nkedmktkfqnq5whnddx5oqpci30x5zjywci31966&rid=giphy.gif&ct=g"
